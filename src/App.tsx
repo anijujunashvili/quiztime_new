0c785e6bcf21c90f56fb3b61aa2da37856872dd7
davit1801 <155184127+davit1801@users.noreply.github.com>
2024-12-23 20:53:12 +0400

    create blog app

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,87 @@
-// import { Button } from "./components/ui/button";
-// import { Card } from "./components/ui/card";
-// import { Progress } from "@radix-ui/react-progress";
-// import { Toggle } from "@radix-ui/react-toggle";
+import { useEffect, useState } from 'react';
+// import { Button } from './components/ui/button';
+// import { Card } from './components/ui/card';
+import { Toggle } from '@radix-ui/react-toggle';
+import { QUIZ } from '@/static';
+import { Button } from '@/components/ui/button';
+import { Progress } from '@/components/ui/progress';
 
 function App() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
   const [isQuizComplete, setIsQuizComplete] = useState(false);
+  const [progressValue, setProgressValue] = useState(25);
 
-  const questions = quiz.questions;
+  const questions = QUIZ.questions;
 
-  const handleAnswerSelection = (isCorrect) => {
+  // const progressValue = (currentQuestionIndex + 1) * 25;
+
+  // useEffect(() => {
+
+  //
+  // }, [currentQuestionIndex, progressValue]);
+
+  console.log(progressValue);
+
+  const handleAnswerSelection = (isCorrect: boolean) => {
     if (isCorrect) {
       setScore(score + 100);
     }
     if (currentQuestionIndex < questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setProgressValue(prev => prev + 25);
     } else {
       setIsQuizComplete(true);
     }
   };
+
   return (
     <>
-      <div>
+      <div className="bg-[#EDE8E3] h-screen flex justify-center items-center  min-w-screen mx-auto">
         {isQuizComplete ? (
           <p>Your score is {score} out of 400</p>
         ) : (
-          <div>
-            <p>{questions[currentQuestionIndex].name}</p>
-            {questions[currentQuestionIndex].variants.map((variant) => (
-              <button
+          <div className="min-h-screen flex min-w-screen flex-col w-[500px] space-y-3 justify-center items-center mx-auto ">
+            <p className="font-semibold text-black text-[24px]">
+              Quiz #{currentQuestionIndex + 1}
+            </p>
+            <p className="font-semibold text-[28px] mb-20 text-[#191D63]">
+              {questions[currentQuestionIndex].name}
+            </p>
+            {questions[currentQuestionIndex].variants.map(variant => (
+              <Toggle
                 key={variant.id}
                 onClick={() => handleAnswerSelection(variant.isCorrect)}
+                className="bg-white w-full rounded-[8px]"
               >
-                {variant.name}
-              </button>
+                <div className="flex gap-5 items-center">
+                  <span className="  bg-[#EDE8E3] w-10 h-10  flex justify-center items-center rounded-full">
+                    {variant.symbol}
+                  </span>
+                  <span> {variant.name}</span>
+                </div>
+              </Toggle>
             ))}
+            {/* <div className='w-full'>
+              <Progress value={progressValue} className="w-7 h-7 bg-blue-800" />
+            </div> */}
+            <div className="p-6 max-w-md mx-auto w-full">
+              {/* <h2 className="text-xl font-semibold mb-4">
+                ShadCN Progress Example
+              </h2> */}
+              <Progress
+                value={progressValue}
+                className=" h-7 bg-[#EDE8E3] bg-white"
+                max={100}
+              />
+              <p className="mt-2 text-sm text-gray-500">
+                {currentQuestionIndex + 1 + '/'}
+                {questions.length}
+              </p>{' '}
+            </div>
           </div>
         )}
       </div>
-      );
     </>
   );
 }
